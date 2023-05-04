c0052628aa6135911af851c324a3768858340ecf
nareshsahu0 <nareshsahu158@gmail.com>
2023-05-04 19:04:35 +0530

    assign

---
+++ day15Programming/MyHashMapTest.java
@@ -0,0 +1,20 @@
+package day15Programming;
+
+import javapractice.MyHashMap;
+
+public class MyHashMapTest 
+{
+	    private static final String MyHashMap = null;
+
+		public void givenAsentence_whenWords() {
+	    	String sentence="To be or not to be";
+	    	new javapractice.MyHashMap();
+	    	MyHashMap<String,Integer>myHashMap=new MyHashMap<>();
+	    	String regex;
+			String[]words=sentence.toLowerCase().split(regex);
+	    	for(String word:words) {
+	    		
+	    	}
+	    }
+
+}
